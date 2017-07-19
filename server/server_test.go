1c8e361b0cc3ac131e96a44c8141c68ab0d398cf
hs <martinh.mireles@gmail.com>
2017-07-19 11:45:25 -0500

    create server_test with ginkgo

--- server/server_test.go
+++ server/server_test.go
@@ -1,32 +1,12 @@
-package server
+package server_test
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
+	. "app/server"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-var req *http.Request
-var err error
+var _ = Describe("Server", func() {
 
-func TestHandler(t *testing.T) {
-	_, err := http.NewRequest(
-		http.MethodGet,
-		"http://localhost:8080/",
-		nil,
-	)
-
-	if err != nil {
-		t.Fatalf("could not create request: %v", err)
-	}
-}
-
-func TestResponseStatusOK(t *testing.T) {
-	rec := httptest.NewRecorder()
-	handler := http.HandlerFunc(RegisterHandlers)
-	handler.ServeHTTP(rec, req)
-
-	if rec.Code != http.StatusOK {
-		t.Errorf("Expected status 200, got: %d", rec.Code)
-	}
-}
+})
