083e13a979f0e01b200acb98050aa224bb696f9a
HS <martinhh.mireles@gmail.com>
2017-07-20 01:33:30 +0000

    comment methods

--- server/server.go
+++ server/server.go
@@ -6,14 +6,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// PathPrefix for server api
 const PathPrefix = "/v1/movies/"
 
+// RegisterHandlers create all server api handlers
 func RegisterHandlers() {
 	r := mux.NewRouter()
 	r.HandleFunc(PathPrefix, ListMovies).Methods("GET")
 	http.Handle(PathPrefix, r)
 }
 
+// ListMovies get a list for dicover movies
 func ListMovies(w http.ResponseWriter, r *http.Request) {
 
 }
