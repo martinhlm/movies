9150c3eca0e3a7f318f7d6ae2e69761372fea737
hs <martinh.mireles@gmail.com>
2017-10-17 18:10:03 -0500

    Add tags field to Promotion model

--- models/promotion.go
+++ models/promotion.go
@@ -2,9 +2,10 @@ package models
 
 // Promotion model
 type Promotion struct {
-	Title    string `bson:"title,omitempty"`
-	Name     string `bson:"name,omitempty"`
-	Category string `bson:"category,omitempty"`
-	Image    string `bson:"image,omitempty"`
-	URL      string `bson:"url,omitempty"`
+	Title    string   `bson:"title,omitempty"`
+	Name     string   `bson:"name,omitempty"`
+	Category string   `bson:"category,omitempty"`
+	Image    string   `bson:"image,omitempty"`
+	URL      string   `bson:"url,omitempty"`
+	Tags     []string `bson:"tags,omitempty"`
 }
