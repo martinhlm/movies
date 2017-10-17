6255c12a8eabc444340cdf72983aeddf805159cc
hs <martinh.mireles@gmail.com>
2017-10-17 15:20:19 -0500

    Add insertion on mongodb

--- database/database.go
+++ database/database.go
@@ -2,15 +2,32 @@ package database
 
 import (
 	"fmt"
+	"movies/models"
 
 	"gopkg.in/mgo.v2"
 )
 
 // Connect to MongoDB database
 func Connect() {
-	_, err := mgo.Dial("localhost")
+	session, err := mgo.Dial("localhost")
 	if err != nil {
 		fmt.Println(err)
+	}
+
+	fmt.Println("Success")
+
+	promotions := session.DB("local").C("promotions")
+	var promotionList = []models.Promotion{
+		{"promo titulo", "promo+titulo", "month", "some_image", "some_url"},
+		{"Promotion title", "Promotion+title", "dynamic", "some_url_image", "other_url"},
+		{"Promotion of dynamic", "Promotion+of+dynamic", "dynamic", "some_url_image", "other_url"},
+	}
+
+	for _, promotion := range promotionList {
+		err := promotions.Insert(promotion)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
 	fmt.Println("Success")
--- models/promotion.go
+++ models/promotion.go
@@ -2,9 +2,9 @@ package models
 
 // Promotion model
 type Promotion struct {
-	Title    string `bson:title`
-	Name     string `bson:name`
-	Category string `bson:category`
-	image    string `bson:image`
-	url      string `bson:url`
+	Title    string `bson:"title,omitempty"`
+	Name     string `bson:"name"`
+	Category string `bson:"category"`
+	Image    string `bson:"image"`
+	URL      string `bson:"url"`
 }
